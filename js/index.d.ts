5ea2fab72efa3e5138dfc4974c1f68939c4d8d46
wchang28 <wchang28@hotmail.com>
2016-08-08 14:40:18 -0400

    first comment

--- js/index.d.ts
+++ js/index.d.ts
@@ -2,3 +2,8 @@ export interface ConnectOptions {
     instance_url?: string;
     rejectUnauthorized?: boolean;
 }
+
+export interface ApiCallOptions {
+	headers?: {[field:string]:string;};
+    rejectUnauthorized?: boolean;
+}
