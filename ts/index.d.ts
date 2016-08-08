ec920abc9bb103a649f6bf3d9499164bcee13aaa
wchang28 <wchang28@hotmail.com>
2016-08-08 14:49:23 -0400

    first comment

--- ts/index.d.ts
+++ ts/index.d.ts
@@ -2,3 +2,8 @@ export interface ConnectOptions {
 	instance_url?: string;
 	rejectUnauthorized?: boolean;
 }
+
+export interface ApiCallOptions {
+	headers?: {[fld:string]:string;};
+    rejectUnauthorized?: boolean;
+}
